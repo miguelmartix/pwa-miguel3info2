08dd7c2f6a80bae03126f60cab77c9fa7b411605
miguelmartix <miguel.martins1030@gmail.com>
2024-02-27 16:42:26 -0300

    instalação do axios/ lebrar de arrumar o erro

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.n7hte12vv3g"
+    "revision": "0.ps1t1v97mlg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
